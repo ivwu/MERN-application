ba02719e509252dbc6d6da6aa419384d64058fca
ivwu22 <ivywu6@gmail.com>
2021-07-11 00:44:23 -0400

    connects mongo and stores password in env file

---
+++ src/server/connect-db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+const dotenv = require("dotenv");
+dotenv.config();
+
+const url = `mongodb+srv://admin:${process.env.DB_PASSWORD}@sei.ivp29.mongodb.net/myorganizer?retryWrites=true&w=majority`;
+let db = null;
+
+export async function connectDB() {
+  if (db) return db;
+  let client = await MongoClient.connect(url, { useNewUrlParser: true });
+  db = client.db();
+  console.log("got db", db);
+  return db;
+}
+
+// connectDB();
