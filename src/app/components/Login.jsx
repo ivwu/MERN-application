349c11f9c46e11daafcc532a2f151b2af1a9da79
ivwu22 <ivywu6@gmail.com>
2021-07-11 14:51:45 -0400

    adds basic styling

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -4,13 +4,13 @@ import {connect} from 'react-redux'
 import * as mutations from '../store/mutations'
 
 const LoginComponent = ({authenticateUser, authenticated}) => (
-  <div>
+  <div className="card p-3 col-6">
     <h2>Please Log In</h2>
     <form onSubmit={authenticateUser}>
-      <input type="text" placeholder="username" name="username" defaultValue="Dev"/>
-      <input type="password" placeholder="password" name="password" defaultValue=""/>
+      <input type="text" placeholder="username" name="username" defaultValue="Dev" className="form-control mt-2"/>
+      <input type="password" placeholder="password" name="password" defaultValue="TUPLES" className="form-control mt-2"/>
       {authenticated === mutations.NOT_AUTHENTICATED ? <p>Login Incorrect</p> : <></>}
-      <button type="submit">Log In</button>
+      <button type="submit" className="form-control mt-2 btn btn-info">Log In</button>
     </form>
   </div>
 )
